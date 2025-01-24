9e95ee3b8d750c07c29fac70ef4ce96b2195d24b
Abdallah Alsammani <85903862+aalsammani@users.noreply.github.com>
2025-01-23 23:10:45 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+function toggleMenu() {
+  const navLinks = document.querySelector('.nav-links');
+  if (navLinks.style.display === 'flex') {
+    navLinks.style.display = 'none';
+  } else {
+    navLinks.style.display = 'flex';
+  }
+}
